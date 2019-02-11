aeda78c58d2fdafc8a05a86488f9fae6e6be72a2
khalid baraka <baraka.khalid4@gmail.com>
2019-02-10 23:40:42 -0500

    Create ChangeThemeTest.java
    #30
    this class is created for testing theme change

---
+++ app/src/androidTest/java/de/test/antennapod/espresso/themeTest/ChangeThemeTest.java
@@ -0,0 +1,95 @@
+package de.test.antennapod.espresso.themeTest;
+
+
+import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.preference.PreferenceManager;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+
+import com.robotium.solo.Solo;
+import com.robotium.solo.Timeout;
+
+import de.danoeh.antennapod.R;
+import de.danoeh.antennapod.activity.PreferenceActivity;
+import de.danoeh.antennapod.core.preferences.UserPreferences;
+
+import org.hamcrest.Matcher;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.shredzone.flattr4j.model.User;
+
+import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static junit.framework.Assert.assertTrue;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.longClick;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+public class ChangeThemeTest {
+
+    private Solo solo;
+    private Resources res;
+    private SharedPreferences prefs;
+
+    @Rule
+    public ActivityTestRule<PreferenceActivity> mActivityRule = new ActivityTestRule<>(PreferenceActivity.class);
+
+    @Before
+    public void setUp() {
+        solo = new Solo(getInstrumentation(), mActivityRule.getActivity());
+        Timeout.setSmallTimeout(500);
+        Timeout.setLargeTimeout(1000);
+        res = mActivityRule.getActivity().getResources();
+        UserPreferences.init(mActivityRule.getActivity());
+
+        prefs = PreferenceManager.getDefaultSharedPreferences(mActivityRule.getActivity());
+        prefs.edit().clear();
+        prefs.edit().putBoolean(UserPreferences.PREF_ENABLE_AUTODL, true).commit();
+    }
+
+    @After
+    public void tearDown() {
+        solo.finishOpenedActivities();
+        prefs.edit().clear();
+    }
+
+    
+
+    private void clickPreference(Matcher<View> matcher) {
+        onView(withId(R.id.list))
+                .perform(RecyclerViewActions.actionOnItem(hasDescendant(matcher), click()));
+    }
+
+    @Test
+    public void testSwitch() {
+        final int theme= UserPreferences.getTheme();
+        final int blueTheme = R.string.pref_theme_title_blue;
+        final int darkTheme = R.string.pref_theme_title_dark;
+        final int blackTheme = R.string.pref_theme_title_trueblack;
+        final int pinkTheme = R.string.pref_theme_title_pink;
+        final int lightTheme = R.string.pref_theme_title_light;
+
+
+            clickPreference(withText(R.string.user_interface_label));
+            clickPreference(withText(R.string.pref_set_theme_title));
+            onView(withText(blueTheme)).perform(click());
+
+
+        assertTrue(solo.waitForCondition(() -> (UserPreferences.getTheme() == theme || UserPreferences.getTheme() != theme), Timeout.getLargeTimeout()));
+    }
+
+}
+
