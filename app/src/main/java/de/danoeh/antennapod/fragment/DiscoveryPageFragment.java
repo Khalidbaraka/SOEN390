b3e64a95a6900e79d9b3b6232881db354238d0c7
TheAlexRobert <alexandrerobertsmeets@gmail.com>
2019-03-01 22:56:40 -0500

    findViewById not working in Fragments, need to use getView().findViewById

--- app/src/main/java/de/danoeh/antennapod/fragment/DiscoveryPageFragment.java
+++ app/src/main/java/de/danoeh/antennapod/fragment/DiscoveryPageFragment.java
@@ -1,15 +1,22 @@
 package de.danoeh.antennapod.fragment;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import de.danoeh.antennapod.R;
+import de.danoeh.antennapod.activity.MainActivity;
+
 import android.os.Bundle;
 
 /**
  * Shows Categories, Top Podcast, & More.
  * Discover your podcast here!
+ *
+ *
+ * Links to categories pages
  */
 
 public class DiscoveryPageFragment extends Fragment {
@@ -25,8 +32,13 @@ public class DiscoveryPageFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        Button categoriesBtn = (Button) getView().findViewById(R.id.CategoriesBtn);
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.discovery_page, container, false);
+
+
     }
 
     @Override
