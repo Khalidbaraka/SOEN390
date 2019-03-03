c681ea103c1b820d47c2733297dc0f4b15deecfe
AleexCh <achung95@hotmail.com>
2019-03-02 23:51:04 -0500

    #58  Fixing failing old tests - PlayBackSonicTest
    New addition of discovery page, messed up the indexing in the test.
    So instead of index 1, needs to be index 2, since Discovery Page took its place.
    
    And fixed code misorder on line 116 117.

--- app/src/androidTest/java/de/test/antennapod/ui/PlaybackSonicTest.java
+++ app/src/androidTest/java/de/test/antennapod/ui/PlaybackSonicTest.java
@@ -33,7 +33,8 @@ import de.danoeh.antennapod.core.storage.PodDBAdapter;
 public class PlaybackSonicTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
     private static final String TAG = PlaybackTest.class.getSimpleName();
-    private static final int EPISODES_DRAWER_LIST_INDEX = 1;
+    //INDEX needs to be updated to 2, since Discovery Page took its place.!
+    private static final int EPISODES_DRAWER_LIST_INDEX = 2;
     private static final int QUEUE_DRAWER_LIST_INDEX = 0;
 
     private Solo solo;
@@ -111,8 +112,9 @@ public class PlaybackSonicTest extends ActivityInstrumentationTestCase2<MainActi
         solo.waitForView(targetView);
         solo.clickOnView(targetView);
         getInstrumentation().waitForIdleSync();
-        solo.waitForText(solo.getString(R.string.all_episodes_short_label));
+        //This was waiting for the string, before clicking it. switched places.
         solo.clickOnText(solo.getString(R.string.all_episodes_short_label));
+        solo.waitForText(solo.getString(R.string.all_episodes_short_label));
         getInstrumentation().waitForIdleSync();
 
         final List<FeedItem> episodes = DBReader.getRecentlyPublishedEpisodes(10);
