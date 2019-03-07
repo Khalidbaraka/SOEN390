0cd36d0c7dbc610eed7d2968ee7f7a841d4834de
Saleh Ali <35427420+SAA9@users.noreply.github.com>
2019-03-07 10:08:07 -0500

    Ui Test Category List Item
    - Press on Itues Button in Discovery Page
    - Assert new page name is iTunes Podcast
    - Select & Open every single category item by their name. (this makes sure the category item exist and u can perform action on it).
    - Combine old 2 tests into 1.

--- app/src/androidTest/java/de/test/antennapod/espresso/category/Categoryfragmenttest.java
+++ app/src/androidTest/java/de/test/antennapod/espresso/category/Categoryfragmenttest.java
@@ -46,7 +46,7 @@ import static org.junit.Assert.assertEquals;
         }
 
         @Test
-        public void test1GoFirstToDiscoveryPage() {
+        public void test1GoFirstToDiscoveryPageVerifyItunesCategoryButton() {
             // queue page
             onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
             String currentPage = getActionbarTitle();
@@ -58,25 +58,118 @@ import static org.junit.Assert.assertEquals;
                 solo.waitForView(android.R.id.list);
                 assertEquals(solo.getString(R.string.discovery_page_label), getActionbarTitle());
             }
-        }
 
-        @Test
-        public void test2ButtonItunesCategories() {
             //Checks button is there
             onView(withId(R.id.itunes_categories_button)).check(matches(notNullValue() ));
 
             //Checks button name matches
             onView(withId(R.id.itunes_categories_button)).check(matches(withText("iTunes Categories")));
             assertEquals("iTunes Categories", solo.getString(R.string.itunes_categories));
+        }
+
+        @Test
+        public void test2FragmentAndTestListItem() {
+
+            //selects & opens the iTunes Categories button in the Discovery Page
+            onView(ViewMatchers.withId(R.id.itunes_categories_button)).perform(click());
+            solo.waitForView(android.R.id.list);
+
+            //Check page text name matches
+            onView(withId(R.id.itunesCategoriesTextView1)).check(matches(withText("iTunes Podcast")));
+            assertEquals("iTunes Podcast", solo.getString(R.string.itunes_podcast));
+
+            //selects & opens the category list item Arts
+            onView(withId(R.id.category_list_id)).perform(RecyclerViewActions.actionOnItem(hasDescendant(withText("Arts")), click()));
+            solo.waitForView(android.R.id.list);
+            pressBack();
+            solo.waitForView(android.R.id.list);
+
+            //selects & opens the category list item Business
+            onView(withId(R.id.category_list_id)).perform(RecyclerViewActions.actionOnItem(hasDescendant(withText("Business")), click()));
+            solo.waitForView(android.R.id.list);
+            pressBack();
+            solo.waitForView(android.R.id.list);
+
+            //selects & opens the category list item Comedy
+            onView(withId(R.id.category_list_id)).perform(RecyclerViewActions.actionOnItem(hasDescendant(withText("Comedy")), click()));
+            solo.waitForView(android.R.id.list);
+            pressBack();
+            solo.waitForView(android.R.id.list);
+
+            //selects & opens the category list item Education
+            onView(withId(R.id.category_list_id)).perform(RecyclerViewActions.actionOnItem(hasDescendant(withText("Education")), click()));
+            solo.waitForView(android.R.id.list);
+            pressBack();
+            solo.waitForView(android.R.id.list);
+
+            //selects & opens the category list item Games & Hobbies
+            onView(withId(R.id.category_list_id)).perform(RecyclerViewActions.actionOnItem(hasDescendant(withText("Games & Hobbies")), click()));
+            solo.waitForView(android.R.id.list);
+            pressBack();
+            solo.waitForView(android.R.id.list);
+
+            //selects & opens the category list item Government & Organization
+            onView(withId(R.id.category_list_id)).perform(RecyclerViewActions.actionOnItem(hasDescendant(withText("Government & Organization")), click()));
+            solo.waitForView(android.R.id.list);
+            pressBack();
+            solo.waitForView(android.R.id.list);
+
+            //selects & opens the category list item Health
+            onView(withId(R.id.category_list_id)).perform(RecyclerViewActions.actionOnItem(hasDescendant(withText("Health")), click()));
+            solo.waitForView(android.R.id.list);
+            pressBack();
+            solo.waitForView(android.R.id.list);
+
+            //selects & opens the category list item Music
+            onView(withId(R.id.category_list_id)).perform(RecyclerViewActions.actionOnItem(hasDescendant(withText("Music")), click()));
+            solo.waitForView(android.R.id.list);
+            pressBack();
+            solo.waitForView(android.R.id.list);
+
+            //selects & opens the category list item News & Politics
+            onView(withId(R.id.category_list_id)).perform(RecyclerViewActions.actionOnItem(hasDescendant(withText("News & Politics")), click()));
+            solo.waitForView(android.R.id.list);
+            pressBack();
+            solo.waitForView(android.R.id.list);
+
+            //selects & opens the category list item Religion & Spirituality
+            onView(withId(R.id.category_list_id)).perform(RecyclerViewActions.actionOnItem(hasDescendant(withText("Religion & Spirituality")), click()));
+            solo.waitForView(android.R.id.list);
+            pressBack();
+            solo.waitForView(android.R.id.list);
+
+            //selects & opens the category list item Science & Medicine
+            onView(withId(R.id.category_list_id)).perform(RecyclerViewActions.actionOnItem(hasDescendant(withText("Science & Medicine")), click()));
+            solo.waitForView(android.R.id.list);
+            pressBack();
+            solo.waitForView(android.R.id.list);
+
+            //selects & opens the category list item Society & Culture
+            onView(withId(R.id.category_list_id)).perform(RecyclerViewActions.actionOnItem(hasDescendant(withText("Society & Culture")), click()));
+            solo.waitForView(android.R.id.list);
+            pressBack();
+            solo.waitForView(android.R.id.list);
+
+            //selects & opens the category list item Sports & Recreation
+            onView(withId(R.id.category_list_id)).perform(RecyclerViewActions.actionOnItem(hasDescendant(withText("Sports & Recreation")), click()));
+            solo.waitForView(android.R.id.list);
+            pressBack();
+            solo.waitForView(android.R.id.list);
+
+            //selects & opens the category list item Technology
+            onView(withId(R.id.category_list_id)).perform(RecyclerViewActions.actionOnItem(hasDescendant(withText("Technology")), click()));
+            solo.waitForView(android.R.id.list);
 
         }
 
-
+        //Method used to press on the back button
+        public static void pressBack() {
+            onView(isRoot()).perform(ViewActions.pressBack());
+        }
 
         private String getActionbarTitle() {
             return ((MainActivity) solo.getCurrentActivity()).getSupportActionBar().getTitle().toString();
         }
-
 
     }
 
