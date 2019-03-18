c40363c7859efd2b0072fe8e783e6fde4890df4a
Jason Chung <jasonckc07@gmail.com>
2019-03-17 22:47:47 -0400

    #104 Created the adapter for the ViewPager to support the tablayout

---
+++ app/src/main/java/de/danoeh/antennapod/adapter/CategoriesViewPagerAdapter.java
@@ -0,0 +1,90 @@
+package de.danoeh.antennapod.adapter;
+
+import android.content.Context;
+import android.os.Parcelable;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CategoriesViewPagerAdapter extends FragmentStatePagerAdapter {
+
+    private final List<Fragment> fragmentList = new ArrayList<>();
+    private final List<String> fragmentListTitle = new ArrayList<>();
+
+    private FragmentManager fragmentManager;
+
+//    private Context myContext;
+
+    public CategoriesViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+        this.fragmentManager = fm;
+//        this.myContext = myContext;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return this.fragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentListTitle.size();
+    }
+
+    @Override
+    public int getItemPosition(Object object) {
+        // Causes adapter to reload all Fragments when
+        // notifyDataSetChanged is called
+        return POSITION_UNCHANGED;
+    }
+
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        fragmentManager.beginTransaction().remove((Fragment)object).commitNowAllowingStateLoss();
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return fragmentListTitle.get(position);
+    }
+
+    public void addFragment(Fragment fragment, String title) {
+        fragmentList.add(fragment);
+        fragmentListTitle.add(title);
+    }
+
+//    @Override
+//    public Object instantiateItem(ViewGroup container, int position) {
+//
+//        Object obj = super.instantiateItem(container, position);
+//        if (obj instanceof  Fragment) {
+//            // record the fragment
+//            Fragment f = (Fragment) obj;
+//            String tag = f.getTag();
+//            fragmentListTitle.add(tag);
+//        }
+//        return obj;
+//    }
+//
+//    public Fragment getFragment(int position) {
+//        String tag = fragmentListTitle.get(position);
+//        if (tag == null) {
+//            return null;
+//        }
+//
+//        return
+//
+//    }
+
+
+    @Override
+    public Parcelable saveState() {
+        return null;
+    }
+}
