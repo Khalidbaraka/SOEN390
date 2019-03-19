1b8a70e3241585e0e84000008d20b8ac6021a9c0
khalid baraka <baraka.khalid4@gmail.com>
2019-03-19 12:18:50 -0400

    fixing a bug in unit test related to feeling lukcy #117
    fixing a bug in unit test related to feeling lukcy #117

--- app/src/androidTest/java/de/test/antennapod/feelingluckytest/FeelingLuckyUnitTests.java
+++ app/src/androidTest/java/de/test/antennapod/feelingluckytest/FeelingLuckyUnitTests.java
@@ -1,19 +1,18 @@
 package de.test.antennapod.feelingluckytest;
 import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.mock;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.json.JSONObject;
 import de.danoeh.antennapod.activity.MainActivity;
 import de.danoeh.antennapod.fragment.FeelingLuckyFragment;
 import de.danoeh.antennapod.model.RandomPodcast;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(AndroidJUnit4.class)
 public class FeelingLuckyUnitTests {
 
 	@Rule
