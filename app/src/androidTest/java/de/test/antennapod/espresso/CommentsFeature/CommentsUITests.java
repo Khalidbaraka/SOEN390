204e0e132bf6551e496d7b7ef5920bb7cc40179f
khalid baraka <baraka.khalid4@gmail.com>
2019-04-02 15:52:05 -0400

    Refactored CommentsUITests file  #185  #120
    Refactored CommentsUITests file #185  #120

--- app/src/androidTest/java/de/test/antennapod/espresso/CommentsFeature/CommentsUITests.java
+++ app/src/androidTest/java/de/test/antennapod/espresso/CommentsFeature/CommentsUITests.java
@@ -1,20 +1,11 @@
 package de.test.antennapod.espresso.CommentsFeature;
-import android.content.SharedPreferences;
-import android.content.res.Resources;
-import android.preference.PreferenceManager;
 import android.support.test.espresso.contrib.DrawerActions;
-import android.support.test.espresso.contrib.NavigationViewActions;
-import android.support.test.espresso.contrib.RecyclerViewActions;
-import android.support.test.filters.FlakyTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.util.Log;
-import android.view.View;
 
 import com.robotium.solo.Solo;
 import com.robotium.solo.Timeout;
 
-import org.hamcrest.Matcher;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.FixMethodOrder;
@@ -25,22 +16,13 @@ import org.junit.runners.MethodSorters;
 
 import de.danoeh.antennapod.R;
 import de.danoeh.antennapod.activity.MainActivity;
-import de.danoeh.antennapod.activity.PreferenceActivity;
-import de.danoeh.antennapod.core.preferences.UserPreferences;
 
 import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.intent.Intents.intended;
-import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
-import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static junit.framework.Assert.assertTrue;
 import static org.hamcrest.CoreMatchers.anything;
-import static org.hamcrest.core.IsNull.notNullValue;
 import static org.junit.Assert.assertEquals;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
@@ -50,7 +32,6 @@ public class CommentsUITests {
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
-
 
     @Before
     public void setUp() {
@@ -64,67 +45,41 @@ public class CommentsUITests {
         solo.finishOpenedActivities();
     }
 
-    //    <string name="add_feed_label">Add Podcast</string>
     @Test
-    public void test1GoFirstt() {
-        // podcast page
+    public void test1() {
+        // Add Podcast page
         onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
-//        onView(withId(R.id.nav_layout))
-//                .perform(NavigationViewActions.navigateTo());
         String currentPage = getActionbarTitle();
         if (("Add Podcast").equals(currentPage)){
-
             assertEquals(solo.getString(R.string.add_feed_label), getActionbarTitle());
-
-
         }else{
-
             solo.clickOnText(solo.getString(R.string.add_feed_label));
             solo.waitForView(R.id.list);
             assertEquals(solo.getString(R.string.add_feed_label), getActionbarTitle());
-
-
         }
-
     }
 
     @Test
-    public void test2GoSecond() {
+    public void test2() {
         // in podcast we want to click on Itunes btn
         onView(withId(R.id.butSearchItunes)).perform(click());
         solo.waitForView(R.id.layout_1);
     }
 
     @Test
-    public void test3GoThird() {
+    public void test3() {
         onView(withId(R.id.butSearchItunes)).perform(click());
         solo.waitForView(R.id.layout_1);
         onData(anything()).inAdapterView(withId(R.id.gridView)).atPosition(0).
                 onChildView(withId(R.id.imgvCover)).perform(click());
-        //clicks on a podcast
+        //clicks on podcast
         solo.waitForView(R.id.relativeLayout);
         onView(withId(R.id.viewCommentsBtn)).perform(click());
         solo.waitForView(R.id.constraintLayout);
-        solo.waitForView(R.id.constraintLayout);
-
 
     }
-
-
-
-
-
-
-
-
-
-
-
-
 
     private String getActionbarTitle() {
         return ((MainActivity) solo.getCurrentActivity()).getSupportActionBar().getTitle().toString();
     }
-
-
 }
