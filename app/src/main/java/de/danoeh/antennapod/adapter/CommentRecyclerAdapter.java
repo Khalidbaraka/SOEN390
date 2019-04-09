5f5fdc0caa678271d96b58f6a013e9745cf98e0a
khalid baraka <baraka.khalid4@gmail.com>
2019-04-09 14:12:33 -0400

    updating the commentRecyclerAdapter #201
    Updated because once we click on
    reply button we need to get the specific comment id so when we want to fetch the data from db, it can be done through comment ID and other attributes as well. #201

--- app/src/main/java/de/danoeh/antennapod/adapter/CommentRecyclerAdapter.java
+++ app/src/main/java/de/danoeh/antennapod/adapter/CommentRecyclerAdapter.java
@@ -1,12 +1,16 @@
 package de.danoeh.antennapod.adapter;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -19,9 +23,14 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 import de.danoeh.antennapod.R;
+import de.danoeh.antennapod.activity.AddCommentActivity;
+import de.danoeh.antennapod.activity.AddReplyActivity;
+import de.danoeh.antennapod.activity.CommentListActivity;
+import de.danoeh.antennapod.activity.ReplyListActivity;
 import de.danoeh.antennapod.model.Comment;
 import de.danoeh.antennapod.model.User;
 
@@ -32,11 +41,15 @@ public class CommentRecyclerAdapter extends RecyclerView.Adapter<CommentRecycler
     private Context context;
     private List <Comment> commentList;
     private List<User> userList;
+
     private FirebaseUser mUser;
     private FirebaseAuth mAuth;
     private DatabaseReference mDatabaseReference;
     private DatabaseReference userReference;
     private FirebaseDatabase mDatabase;
+    private DatabaseReference mCommentDatabase;
+
+
 
     public CommentRecyclerAdapter(Context context, List<Comment> commentList) {
         this.context = context;
@@ -44,6 +57,8 @@ public class CommentRecyclerAdapter extends RecyclerView.Adapter<CommentRecycler
         this.userList = new ArrayList<>();
         mDatabase=FirebaseDatabase.getInstance();
         userReference=mDatabase.getReference().child("users");
+        mCommentDatabase=mDatabase.getReference().child("Comment");
+
     }
 
     @NonNull
@@ -77,6 +92,7 @@ public class CommentRecyclerAdapter extends RecyclerView.Adapter<CommentRecycler
                     User user= new User();
                     user.setEmail(d.child("email").getValue(String.class));
                     user.setFullName(d.child("fullName").getValue(String.class));
+
                     userList.add(user);
                 }
 
@@ -85,6 +101,10 @@ public class CommentRecyclerAdapter extends RecyclerView.Adapter<CommentRecycler
                     if(u.getEmail().equals(comment.useremail)){
                         Log.d("user name is :", u.getFullName());
                         holder.userName.setText(u.getFullName());
+                        holder.userEmail= u.getEmail();
+                        holder.podcast= comment.getPodcast();
+                        holder.commentID= comment.toString();
+//                        holder.image= u.
                         break;
                     }
                 }
@@ -96,6 +116,7 @@ public class CommentRecyclerAdapter extends RecyclerView.Adapter<CommentRecycler
 
             }
         });
+
     }
 
     @Override
@@ -108,21 +129,76 @@ public class CommentRecyclerAdapter extends RecyclerView.Adapter<CommentRecycler
         public TextView comment;
         public TextView timestamp;
         public TextView userName;
+        public Button addReplyBtn;
         public String userId;
-
+        public String userEmail;
+        public String podcast;
+        public String commentID;
+        public String commentIDFROMDB;
+        public ImageButton image;
         public ViewHolder(@NonNull View view, Context ctx ) {
             super(view);
             context = ctx;
             comment = (TextView)view.findViewById(R.id.commentTitleList);
             timestamp = (TextView)view.findViewById(R.id.timestampList);
             userName= (TextView)view.findViewById(R.id.userName);
+            addReplyBtn = (Button)view.findViewById(R.id.addReplyBtn);
+            image= (ImageButton)view.findViewById(R.id.imageButton3);
             userId = null;
             view.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     // we can go to nxt activity
+
                 }
             });
+
+            addReplyBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    mCommentDatabase.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            Iterator<DataSnapshot> comments = dataSnapshot.getChildren().iterator();
+                            while (comments.hasNext()){
+                                DataSnapshot item = comments.next();
+
+
+                                if( (item.child("comment").getValue().equals(comment.getText().toString())) &&
+                                        (item.child("podcast").getValue().equals(podcast)) &&
+                                        (item.child("useremail").getValue().equals(userEmail))
+                                ){
+                                    commentIDFROMDB= item.getKey().toString();
+                                    Log.d("the comment id from DB ", commentIDFROMDB);
+                                    break;
+
+                                }
+                            }
+
+                            Intent intent= new Intent(context.getApplicationContext(), AddReplyActivity.class);
+                            intent.putExtra("comment",comment.getText().toString());
+                            intent.putExtra("commentID", commentIDFROMDB);
+                            intent.putExtra("userEmail",userEmail);
+                            intent.putExtra("commentOwner", userName.getText().toString());
+                            intent.putExtra("podcast",podcast);
+                            context.startActivity(intent);
+                            ((Activity)context).finish();
+
+                        }
+
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+
+
+
+                }
+            });
+
         }
     }
 }
