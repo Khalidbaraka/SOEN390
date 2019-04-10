b14065b341b6e21bca0f66e00b253d93ac65391b
Dorin <dorin.rogov@gmail.com>
2019-04-09 21:36:28 -0400

    #188 added debugging print statement to python script

--- CI-Analysis/analyze_ci.py
+++ CI-Analysis/analyze_ci.py
@@ -6,7 +6,15 @@ successKeywords = ["success", "complete", "pass"]
 failureKeywords = ["error", "fail"]
 generalExecutionKeywords = ["executed"]
 
+allSuccessMessages = []
+allFailureMessage = []
+allExecutionMessages = []
+
+print("dorins-python-script-was-run")
 
 for line in sys.stdin:
     print(line)
+    #add the logic in to get all our stats in here and then print the out at the end.
+    #this will be better than just outputting are the random stuff from the travis builds
+    
 
