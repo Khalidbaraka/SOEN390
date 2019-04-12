7721f91a9c6da632ef1277e6b38d5bdfad6096d6
Dorin <dorin.rogov@gmail.com>
2019-04-12 16:01:15 -0400

    #188 testing log analysis functionality

--- CI-Analysis/analyze_ci.py
+++ CI-Analysis/analyze_ci.py
@@ -10,10 +10,10 @@ allSuccessMessages = []
 allFailureMessage = []
 allExecutionMessages = []
 
-print("dorins-python-script-was-run")
+print("dorin-test-2-no-log-printing")
 
 for line in sys.stdin:
-    print(line)
+    #print(line)
     #add the logic in to get all our stats in here and then print the out at the end.
     #this will be better than just outputting are the random stuff from the travis builds
     
